eec391f6190a69fcb877ccd46243f0ee9715a90e
MzadeTural <tu201906140@code.edu.az>
2024-02-18 17:49:58 +0400

    AddDataAccesServices Fixed

---
+++ src/Doczy.DataAccess/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Doczy.DataAccess.Configurations
+{
+    public class ServiceConfiguration
+    {
+        public static string ConnectionString()
+        {
+
+            ConfigurationManager manager = new ConfigurationManager();
+            manager.SetBasePath(Directory.GetCurrentDirectory() + "../../RentaCar.Api");
+            manager.AddJsonFile("appsettings.json");
+            return manager.GetConnectionString("Default");
+        }
+    }
+}
--- src/Doczy.DataAccess/Repositories/ServiceRegistration.cs
+++ src/Doczy.DataAccess/Repositories/ServiceRegistration.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+
+
+using Doczy.Core.Entities.Identities;
+using Doczy.DataAccess.Configurations;
+using Doczy.DataAccess.Contexts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Doczy.DataAccess.Repositories
 {
-    internal class ServiceRegistration
+    public static class ServiceRegistration
     {
+
+        public static void AddDataAccesServices(this IServiceCollection services)
+        {
+            services.AddDbContext<DoczyContext>(opt =>
+            {
+                opt.UseSqlServer(ServiceConfiguration.ConnectionString());
+            }).AddIdentity<AppUser, IdentityRole<Guid>>(x =>
+            {
+                x.Password.RequiredLength = 8;
+               
+                x.SignIn.RequireConfirmedEmail = true;
+                x.User.RequireUniqueEmail = true;
+               
+            }).
+            AddEntityFrameworkStores<DoczyContext>().AddDefaultTokenProviders();
+
+           
+
+        }
+
     }
 }
