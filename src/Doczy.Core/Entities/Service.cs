123b9bc276dbaea0520e627fe805722961594091
MzadeTural <tu201906140@code.edu.az>
2024-02-20 13:42:52 +0400

    Create  Service,ServiceType Entity

---
+++ src/Doczy.Core/Entities/Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doczy.Core.Entities
+{
+    internal class Service
+    {
+    }
+}
