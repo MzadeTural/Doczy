3ebfbcdb81b2dadd5691fa47e9d731c9052ab2f1
MzadeTural <tu201906140@code.edu.az>
2024-02-21 23:13:57 +0400

    Configurating Mapper

---
+++ src/Doczy.Business/MappingProfiles/DoctorMapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Doczy.Business.DTOs.UserDtos;
+using Doczy.Core.Entities.Identities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doczy.Business.MappingProfiles
+{
+    public class DoctorMapper :Profile
+    {
+        public DoctorMapper()
+        {
+               
+        CreateMap<CreateDoctorDto, DoctorAppUser>()
+                .ForMember(usc => usc.DiplomaImageUrl, e => e.Ignore())
+                .ForMember(usc => usc.IdCardImageUrl, e => e.Ignore())              
+                .ReverseMap();
+             }
+    }
+}
