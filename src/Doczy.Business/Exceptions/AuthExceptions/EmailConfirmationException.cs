77baf02a591058c36aa7cd675575aedb1d58b6c5
MzadeTural <tu201906140@code.edu.az>
2024-02-26 14:42:53 +0400

    Confirm email service dtoS and string exception extension

---
+++ src/Doczy.Business/Exceptions/AuthExceptions/EmailConfirmationException.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Net;
+
+namespace Doczy.Business.Exceptions.AuthExceptions
+{
+    public class EmailConfirmationException : Exception, IBaseException
+    {
+        public HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
+
+        public string ErrorMessage { get; }
+        public string? errorDetail { get; }
+
+        public EmailConfirmationException()
+        {
+            ErrorMessage = "Unexpected error occurred while activate user";
+        }
+
+        public EmailConfirmationException(IEnumerable<IdentityError> errors)
+        {
+            ErrorMessage = "Unexpected error occurred while activate user";
+            errorDetail = String.Join(',', errors.Select(e => e.Description));
+        }
+
+        public EmailConfirmationException(string message) : base(message)
+        {
+            ErrorMessage = message;
+        }
+
+        public EmailConfirmationException(string message, Exception? innerException) : base(message, innerException)
+        {
+            ErrorMessage = message;
+        }
+
+
+    }
+}
---
+++ src/Doczy.Business/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Doczy.Business.DTOs.Common;
+using Doczy.Business.DTOs.UserDtos;
+
+namespace Doczy.Business.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        public Task<ResponseDto> ConfirmEmailAsync(ConfirmEmailDto confirmEmailDto);
+    }
+}
