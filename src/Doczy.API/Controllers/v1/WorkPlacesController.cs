ccdff9bb7389d44f1eac943632b2d153cc4e8460
MzadeTural <tu201906140@code.edu.az>
2024-02-27 00:42:03 +0400

    Create work place controller

---
+++ src/Doczy.API/Controllers/v1/WorkPlacesController.cs
@@ -0,0 +1,32 @@
+﻿using Doczy.Business.DTOs.Common;
+using Doczy.Business.DTOs.ServiceDtos;
+using Doczy.Business.DTOs.WorkPlace;
+using Doczy.Business.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Doczy.API.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkPlacesController : ControllerBase
+    {
+        private readonly IWorkPlaceService _workPlaceService;
+
+        public WorkPlacesController(IWorkPlaceService workPlaceService)
+        {
+            _workPlaceService = workPlaceService;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        //[Authorize(AuthenticationSchemes = "Bearer", Roles = "Doctor")]
+        public async Task<IActionResult> Create([FromForm] CreateWorkPlaceDto createWPDto)
+        {
+
+            var response = await _workPlaceService.CreateWorkPlaceAsync(createWPDto);
+            return StatusCode((int)HttpStatusCode.OK, new ResponseDto(response.StatusCode, response.Message));
+
+        }
+    }
+}
