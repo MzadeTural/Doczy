30241b21bfead2e359ae733fca6bd9338547929f
Orkhan Ganbarov <tu201906147@code.edu.az>
2024-03-07 00:14:23 +0400

    Added_Service

---
+++ src/Doczy.API/Controllers/v1/UnivercitiesController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Doczy.API.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnivercitiesController : ControllerBase
+    {
+    }
+}
