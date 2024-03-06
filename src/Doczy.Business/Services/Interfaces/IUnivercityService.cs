b863da3f659ebc220e560fbcd944773e16436362
Orkhan Ganbarov <tu201906147@code.edu.az>
2024-03-06 23:17:53 +0400

    Create Univercity Repo and Dtos

---
+++ src/Doczy.Business/Services/Interfaces/IUnivercityService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Doczy.Business.DTOs.Common;
+using Doczy.Business.DTOs.UnivercityDtos;
+
+namespace Doczy.Business.Services.Interfaces
+{
+	public interface IUnivercityService
+	{
+        Task<ResponseDto> CreateUnivercityAsync(CreateUnivercityDto model);
+        Task<ResponseDto> CreateUnivercityAsync(Guid id, UpdateUnivercityDto model);
+        Task<ResponseDto> CreateUnivercityAsync(Guid id);
+    }
+}
+
