ba198350d9cd746fe35ff04db0ca08401f8d7ca7
MzadeTural <tu201906140@code.edu.az>
2024-03-07 13:32:23 +0400

    Create Doctor Raiting service and Dto

--- src/Doczy.Business/Services/Interfaces/IDoctorService.cs
+++ src/Doczy.Business/Services/Interfaces/IDoctorService.cs
@@ -1,6 +1,7 @@
 ﻿using Doczy.Business.DTOs.Common;
 using Doczy.Business.DTOs.DoctorDtos;
 using Doczy.Business.DTOs.Language;
+using Doczy.Business.DTOs.RaitingDtos;
 
 namespace Doczy.Business.Services.Interfaces
 {
@@ -9,6 +10,7 @@ namespace Doczy.Business.Services.Interfaces
         Task<List<GetDoctorAppointmentsDto>> GetDoctorAppointments();
         Task<List<GetDoctorsDto>> GetFilterDoctors(GetDoctorFilterDto model);
         Task<ResponseDto> UpdatePhoneNumberAsync( UserPhoneUpdateDto model);
+        Task<ResponseDto> RaitingDoctor( CreateRaitingDto model);
         Task<ResponseDto> UpdateWorkPlaceAsync(Guid id, Guid worpPlaceId);
         Task<ResponseDto> AddLanguageAsync(Guid languageId);
         Task<ResponseDto> UpdateCategoryAsync(Guid categoryId);
