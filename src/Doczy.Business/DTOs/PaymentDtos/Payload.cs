f4101af56c88eed721314b169101546cc2f112b8
Orkhan Ganbarov <tu201906147@code.edu.az>
2024-04-22 14:28:35 +0400

    salam-sagol

--- src/Doczy.Business/DTOs/PaymentDtos/Payload.cs
+++ src/Doczy.Business/DTOs/PaymentDtos/Payload.cs
@@ -16,7 +16,7 @@ namespace Doczy.Business.DTOs.PaymentDtos
         public string PAN { get; set; }
         public decimal PurchaseAmount { get; set; }
         public string Currency { get; set; }
-        public DateTime TranDateTime { get; set; }
+        public string TranDateTime { get; set; } ///
         public string ResponseCode { get; set; }
         public string ResponseDescription { get; set; }
         public string Brand { get; set; }
@@ -26,7 +26,7 @@ namespace Doczy.Business.DTOs.PaymentDtos
         public string ApprovalCodeScr { get; set; }
         public decimal PurchaseAmountScr { get; set; }
         public string CurrencyScr { get; set; }
-        public CardRegistration CardRegistration { get; set; }
+        //public CardRegistration CardRegistration { get; set; }
         public string InvoiceUuid { get; set; }
     }
 }
