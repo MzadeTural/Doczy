96e5f7f2e2c1cd378e7765f9e0f53d99e24e8446
Orkhan Ganbarov <tu201906147@code.edu.az>
2024-04-29 00:03:48 +0400

    Repo blog done

---
+++ src/Doczy.DataAccess/Repositories/Implementations/BlogRepository.cs
@@ -0,0 +1,15 @@
+﻿using Doczy.Core.Entities;
+using Doczy.DataAccess.Contexts;
+using Doczy.DataAccess.Repositories.Implementations.Base;
+using Doczy.DataAccess.Repositories.Interfaces;
+
+namespace Doczy.DataAccess.Repositories.Implementations
+{
+	public class BlogRepository:Repository<Blog>,IBlogRepository
+	{
+		public BlogRepository(DoczyContext context):base(context)
+		{
+		}
+	}
+}
+
