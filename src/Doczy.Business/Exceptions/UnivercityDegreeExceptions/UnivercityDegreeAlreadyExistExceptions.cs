afd83aeddaf37f3bcb6b276500a3b28fbc368a71
Orkhan Ganbarov <tu201906147@code.edu.az>
2024-05-02 00:51:56 +0400

    Uni Degree done

---
+++ src/Doczy.Business/Exceptions/UnivercityDegreeExceptions/UnivercityDegreeAlreadyExistExceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace Doczy.Business.Exceptions.UnivercityDegreeExceptions
+{
+	public class UnivercityDegreeAlreadyExistExceptions : Exception, IBaseException
+    {
+        public HttpStatusCode StatusCode => HttpStatusCode.SeeOther;
+
+        public string ErrorMessage { get; }
+
+        public UnivercityDegreeAlreadyExistExceptions(string Message) : base(Message) { ErrorMessage = Message; }
+    }
+}
+
---
+++ src/Doczy.Business/Exceptions/UnivercityDegreeExceptions/UnivercityDegreeNotFoundExceptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace Doczy.Business.Exceptions.UnivercityDegreeExceptions
+{
+	public class UnivercityDegreeNotFoundExceptions : Exception, IBaseException
+    {
+        public HttpStatusCode StatusCode => HttpStatusCode.SeeOther;
+
+        public string ErrorMessage { get; }
+
+        public UnivercityDegreeNotFoundExceptions(string Message) : base(Message) { ErrorMessage = Message; }
+    }
+}
