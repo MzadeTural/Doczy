611640c771b8b92968f16e7be57bea8211d680d9
Orkhan Ganbarov <tu201906147@code.edu.az>
2024-05-03 01:15:59 +0400

    payment data

---
+++ src/Doczy.Core/Entities/PayriffPayments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Doczy.Core.Entities.Common;
+
+namespace Doczy.Core.Entities
+{
+	public class PayriffPayments : BaseAuditableEntity
+    {
+        public Guid AppointmentId { get; set; }
+        public Appointment Appointment { get; set; }
+        public string RRN { get; set; }
+        public int OrderId { get; set; }
+        public string SessionId { get; set; }
+        public string TranDateTime { get; set; }
+        public string OrderStatus { get; set; }
+        public string ApprovalCodeScr { get; set; }
+        public decimal PurchaseAmount { get; set; }
+    }
+}
+
