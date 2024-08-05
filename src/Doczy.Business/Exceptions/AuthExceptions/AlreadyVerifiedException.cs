d2ee8f2a919040d6d21388f095782734e2d43ff1
MzadeTural <tu201906140@code.edu.az>
2024-08-05 12:37:12 +0400

    Fixed bug VerifiedDoctorService

---
+++ src/Doczy.Business/Exceptions/AuthExceptions/AlreadyVerifiedException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doczy.Business.Exceptions.AuthExceptions
+{
+    
+    public class AlreadyVerifiedException : Exception, IBaseException
+    {
+
+
+        public HttpStatusCode StatusCode { get; }
+
+        public string ErrorMessage { get; }
+
+
+
+        public AlreadyVerifiedException(string message, HttpStatusCode statusCode) : base(message)
+        {
+            StatusCode = statusCode;
+            ErrorMessage = message;
+        }
+
+        public AlreadyVerifiedException(string message, HttpStatusCode statusCode, Exception? innerException) : base(message, innerException)
+        {
+            StatusCode = statusCode;
+            ErrorMessage = message;
+        }
+    }
+}
